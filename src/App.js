ce116c29d2330679c807e03a1aaa368652436e38
Matthias Hutzler <matthias-hutzler@posteo.net>
2022-12-03 13:01:54 +0100

    multiplayer...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Client } from 'boardgame.io/react';
+import { SocketIO } from 'boardgame.io/multiplayer';
 import { Gomoku } from './Game';
 import { GomokuBoard } from './Board';
 
-const App = Client({
+const RawApp = Client({
   game: Gomoku,
   board: GomokuBoard,
+  multiplayer: SocketIO({ server: 'localhost:8000' }),
 });
+
+const App = _ => (
+  <div>
+    <RawApp playerID="0" />
+  </div>
+);
 
 export default App;
